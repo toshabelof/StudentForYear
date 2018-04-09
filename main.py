8b02950411c8553f2fae3f5bde01b8c3ab3f2144
vosyukov <osyukov.vadim@gmail.com>
2018-04-10 01:34:42 +0300

    правки

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import time
 import datetime
 import random
+import json
 
 """
 vk = vk_api.VkApi(token='169428aad53f73252e0b465e1f2f899c10df80e9024e4f80aa8f9783c5d5a860effa0a20292618fa50c54')
@@ -139,37 +141,69 @@ print(listStudent)
 print(listUser_id)
 """
 
-
 import requests
 
-users = []
+members = []
 
-users[0] = {'surname':'Осюков', 'str':'Осюков'}
+members.append({'id': 0, 'surname': 'Осюков', 'str': 'Осюков'})
 
 group_token = "25e4b62e76e29d2f67a26d27bf43046f74654d030dc85a2b9535b63177da5301a82e53d30e9de24caa805"
 
 
 def sendMessage(user_id, message):
-    response = call_vk_api("messages.send", {'user_id': user_id, 'message': message})
-    return response
+	response = call_vk_api("messages.send", {'user_id': user_id, 'message': message})
+	return response
 
 
 def getLongPollServer():
-    response = call_vk_api('groups.getLongPollServer', {'group_id': 117644300})
-    return response
+	response = call_vk_api('groups.getLongPollServer', {'group_id': 117644300})
+	return response
 
 
 def getLongPollEvents(server, key, ts):
-    response = requests.post("%s?act=a_check&key=%s&ts=%s&wait=25" % (server, key, ts))
-    return response.json()
+	response = requests.post("%s?act=a_check&key=%s&ts=%s&wait=25" % (server, key, ts))
+	return response.json()
 
 
 def call_vk_api(method, params):
-    method = "https://api.vk.com/method/%s" % (method)
-    params['access_token'] = group_token
-    params['v'] = '5.74'
-    response = requests.post(method, params).json()['response']
-    return response
+	method = "https://api.vk.com/method/%s" % (method)
+	params['access_token'] = group_token
+	params['v'] = '5.74'
+	response = requests.post(method, params).json()['response']
+	return response
+
+
+# сохраняет голос в файл
+def save_vote(user_id, member_id):
+	f = open('votes', 'a')
+	f.write(json.dumps({'user_id': user_id, 'member_id': member_id, 'date': int(time.time())}) + '\n')
+	f.close()
+
+
+# функция проверяет головал ли пользователь ранее
+def is_vote(user_id):
+	result = False
+	f = open('votes')
+	for line in f:
+		obj = json.loads(line)
+		if obj['user_id'] == user_id:
+			result = True
+			break
+	f.close()
+	return result
+
+	f.write(json.dumps({'user_id': user_id, 'member_id': member_id, 'date': time.time()}) + '\n')
+	f.close()
+
+# функция парсит сообщение и возвращает id  участника или none
+def getMemberByMessage(message):
+	message = message.lower()
+	for m in members:
+		if message.find(m['str'].lower()) != -1:
+			return m
+
+	return None
+
 
 
 
@@ -177,109 +211,34 @@ def call_vk_api(method, params):
 params = getLongPollServer()
 
 
-# список в словарь
-def fileUpdateList(i):
-    while True:
-        try:
-            listStudent.update({file[i]: int(file[i + 1])})
-            i += 2
-        except:
-            break
-
-
-def newUserID(user_id):
-    userID_odl = False
-    for l in listUser_id:
-        if user_id == int(l):
-            userID_odl = True
-            break
-    if userID_odl:
-        return True
-    else:
-        return False
-
-other_bot = ''
-
-# Обработка голоса юзера
-def update(Golos):
-    global other_bot
-    for l in listStudent:
-        if Golos == l:
-            if newUserID(event['object']['user_id']) == False:
-                listUser_id.append(str(event['object']['user_id']))
-                other = listStudent.get(l) + 1
-                listStudent.update({l: other})
-
-                # Пишев в файл результаты
-                with open('list.txt', 'w') as csv_file:
-                    for item in listStudent:
-                        csv_file.write("%s\n%s\n" % (item, listStudent.get(item)))
-                    f.close()
-
-                with open('user_id.txt', 'w') as csv_file:
-                    for item in listUser_id:
-                        csv_file.write("%s\n" % (item))
-                    f.close()
-
-                sendMessage(event['object']['user_id'], "Ваш голос принят!!")  # отправка сообщения
-                other_bot = 'Ваш голос принят!'
-                return True
-                break
-            else:
-                sendMessage(event['object']['user_id'], "Вы уже голосовали!!")  # отправка сообщения
-                other_bot = 'Вы уже голосовали!!'
-                return True
-                break
-    return False
 
 
 
 while (True):
-    try:
-        # считываем из файла
-        with open("list.txt", "r") as f:
-            file = f.read().splitlines()
-            f.close()
+	try:
 
-        listStudent = {}
-        i = 0
-        fileUpdateList(i)
-        print(listStudent)
+		result = getLongPollEvents(params['server'], params['key'], params['ts'])
 
-        with open("user_id.txt", "r") as f:
-            listUser_id = f.read().splitlines()
-            f.close()
-        print(listUser_id)
+		params['ts'] = result['ts']
 
-        result = getLongPollEvents(params['server'], params['key'], params['ts'])
-        if str(result['updates']) == '':
-            break
+		events = result['updates']
 
-        params['ts'] = result['ts']
+		for event in events:
+			if event['type'] == 'message_new':
+				user_id = event['object']['user_id']
+				body = event['object']['body']
 
-        events = result['updates']
+				if (is_vote(user_id)):
+					sendMessage(user_id, 'Вы уже головали')
+				else:
+					member = getMemberByMessage(body)
 
-        for event in events:
-            if event['type'] == 'message_new':
-                if str(event['object']['body']).lower() == "привет":
-                    sendMessage(event['object']['user_id'], "Привет! Твоя задача написать мне фамилию того студента, который по твоему мнению заслуживает приз зрительских симпатий:\n"+
-                                                            "1. Павел Табунов\n" +
-                                                            "2. Никита Иванов\n" +
-                                                            "3. Дарья Филатьева\n" +
-                                                            "4. Екатерина Блинова")
-                    print('\n' + str(datetime.datetime.now().strftime('%d/%m/%Y %H:%M:%S')) + ' Пишет: ' + str(event['object']['user_id']) + ' "' + str(event['object']['body']) + '"\n')
+					if member:
+						save_vote(user_id, member['id'])
+						sendMessage(user_id, 'Ваш голос принят')
 
-                else:
-                    user_golos = update(event['object']['body'])
+					else:
+						sendMessage(user_id, 'Отправьте фамилию участника за которого хотели бы проголовать')
 
-                    if user_golos != True:
-                        sendMessage(event['object']['user_id'], "Увы, такого участника нет!!")  # отправка сообщения
-                        other_bot = "Увы, такого участника нет!!"
-
-                    print('\n' + str(datetime.datetime.now().strftime('%d/%m/%Y %H:%M:%S')) + ' Проголосовал: ' + str(event['object']['user_id']) + ' за '
-                              + str( event['object']['body']) + '. Бот ответил: "' + other_bot + '"\n')
-
-    except Exception as e:
-        print(e)
-
-
+	except Exception as e:
+		print(e)
